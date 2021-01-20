2f2f07a9cbd6de10eea96b919fef1a84d3853b06
Liyuan He <woaihkx@gmail.com>
2021-01-19 23:42:45 -0600

    sticky navbar

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,22 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyFunction()};
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyFunction() {
+  // Get the navbar
+  var navbar = document.getElementById("myTopnav");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  if (window.pageYOffset >= sticky) {
+    //x.className += " responsive";
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
