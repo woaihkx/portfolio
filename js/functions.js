1bd2a520f643d2019530516a1c356ba07fd07087
Liyuan He <woaihkx@gmail.com>
2021-01-20 11:16:18 -0600

    add sticky and opacity to mobile navbar

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "topnav sticky") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "topnav sticky";
   }
 }
 
